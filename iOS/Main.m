62e2ca233fb0423323f8bc9f215f9cf9f1f84598
Plasmo <bowmanjordyn08@gmail.com>
2026-07-03 20:34:01 -0500

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -1,9 +1,7 @@
-// Copyright (c) 2025 Project Nova LLC
-
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"https://api.private-exal.xyz"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
@@ -11,8 +9,8 @@
 
 @implementation CustomURLProtocol
 
-+ (BOOL)canInitWithRequest:(NSURLRequest *)request 
-{  
++ (BOOL)canInitWithRequest:(NSURLRequest *)request
+{
     NSString *absoluteURLString = [[request URL] absoluteString];
     if ([absoluteURLString containsString:EPIC_GAMES_URL] && ![absoluteURLString containsString:@"/CloudDir/"]) {
         if ([NSURLProtocol propertyForKey:@"Handled" inRequest:request]) {
@@ -23,48 +21,60 @@
     return NO;
 }
 
-+ (NSURLRequest*)canonicalRequestForRequest:(NSURLRequest*)request
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request
 {
     return request;
 }
 
 - (void)startLoading
 {
-    NSMutableURLRequest* modifiedRequest = [[self request] mutableCopy];
+    NSMutableURLRequest *modifiedRequest = [[self request] mutableCopy];
 
-    NSString* originalPath = [modifiedRequest.URL path];
-    NSString* newBaseURLString = API_URL;
+    NSString *originalPath = [modifiedRequest.URL path];
+    NSString *newBaseURLString = API_URL;
 
-    NSURLComponents* components = [NSURLComponents componentsWithString:newBaseURLString];
+    NSURLComponents *components = [NSURLComponents componentsWithString:newBaseURLString];
     components.path = originalPath;
 
-    NSURLComponents *originalComponents = [NSURLComponents componentsWithURL:modifiedRequest.URL resolvingAgainstBaseURL:NO];
+    NSURLComponents *originalComponents =
+        [NSURLComponents componentsWithURL:modifiedRequest.URL
+                  resolvingAgainstBaseURL:NO];
+
     if (originalComponents.queryItems.count > 0) {
         NSMutableArray<NSURLQueryItem *> *cleanItems = [NSMutableArray array];
+
         for (NSURLQueryItem *item in originalComponents.queryItems) {
-            NSString *decodedValue = item.value ? [item.value stringByRemovingPercentEncoding] : nil;
-            [cleanItems addObject:[NSURLQueryItem queryItemWithName:item.name value:decodedValue]];
+            NSString *decodedValue =
+                item.value ? [item.value stringByRemovingPercentEncoding] : nil;
+
+            [cleanItems addObject:[NSURLQueryItem queryItemWithName:item.name
+                                                              value:decodedValue]];
         }
+
         components.queryItems = cleanItems;
     }
 
     [modifiedRequest setURL:components.URL];
-    [NSURLProtocol setProperty:@YES forKey:@"Handled" inRequest:modifiedRequest];
+    [NSURLProtocol setProperty:@YES
+                        forKey:@"Handled"
+                     inRequest:modifiedRequest];
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnonnull"
     [[self client] URLProtocol:self
-        wasRedirectedToRequest:modifiedRequest
-              redirectResponse:nil];
+      wasRedirectedToRequest:modifiedRequest
+            redirectResponse:nil];
 #pragma clang diagnostic pop
 }
 
 - (void)stopLoading
 {
 }
+
 @end
 
 __attribute__((constructor)) void entry()
 {
     [NSURLProtocol registerClass:[CustomURLProtocol class]];
 }
+
